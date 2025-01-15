d2c0200d3e588a5a223660326cb6aed156d68f79
Christopher Lackner <clackner@cerbsim.com>
2025-01-15 16:15:32 +0100

    draw function from pyodide magic

--- webgpu/__init__.py
+++ webgpu/__init__.py
@@ -1,5 +1,7 @@
 from .gpu import WebGPU
-from .utils import BaseBinding, BufferBinding, read_shader_file
+from .utils import (BaseBinding, BufferBinding, read_shader_file, UniformBinding,
+                    create_bind_group)
 from .render_object import RenderObject
 from .draw import Draw
 from .font import Font
+from .colormap import Colormap
